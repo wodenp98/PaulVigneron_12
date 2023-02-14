fb20acd1e81f231975e0b055a8cb17fc3bd555ce
Paul VIGNERON <paulvigneron4698@gmail.Com>
2023-02-14 17:20:12 +0100

    set up depencies and projet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Error from "./Components/Error";
+import Profil from "./Components/Profil";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/12" element={<Profil />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import normalize from "normalize.css";
+import { Helmet } from "react-helmet";
 
+const GlobalStyle = createGlobalStyle`
+${normalize}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+  body {
+    padding: 0;
+    font-family: "Roboto", sans-serif;
+  }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
-
